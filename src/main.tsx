cca0c6e7612ec7e1e392426357fe655e54c7faa8
Thiago Santos <virtualves@gmail.com>
2024-12-01 16:56:47 -0400

    Index routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoices/index.tsx";
 import NotFound from "./routes/NotFound/index.tsx";
 import Invoice from "./routes/Invoices/Invoice/index.tsx";
+import InvoicesIndex from "./routes/Invoices/InvoicesIndex/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} >
+          <Route index element={<InvoicesIndex />} />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route path="*" element={<NotFound />}
